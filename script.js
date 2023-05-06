6a946c5517c0888a55d0125cbdfdd27e706e3bee
ítalo Fabrício <italofps65@gmail.com>
2023-05-06 18:57:18 -0300

    feat: finalizando o carrinho no desktop

--- script.js
+++ script.js
@@ -102,8 +102,16 @@ function updateCart(){
     if(cart.length > 0){
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
+
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
         for(let i in cart){
             let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt;
+
+            
             let cartItem = c('.cart--item').cloneNode(true);
 
             let pizzaSizeName;
@@ -124,9 +132,30 @@ function updateCart(){
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                if(cart[i].qt > 1){
+                    cart[i].qt--;
+                } else{
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                cart[i].qt++;
+                updateCart();
+            });
+            
             c('.cart').append(cartItem);
 
         }
+
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2).replace('.', ',')}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2).replace('.', ',')}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2).replace('.', ',')}`;
+
     } else{
         c('aside').classList.remove('show');
     }
