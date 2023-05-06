0e71e6f5d9e4830607c5823e16f87430d66d208c
ítalo Fabrício <italofps65@gmail.com>
2023-05-06 02:20:29 -0300

    feat: adicionando as pizzas ao carrinho e verificando pizzas repetidas

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -18,6 +20,7 @@ pizzaJson.map((item, index) => {
         e.preventDefault(); //previna a ação padrão
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -74,3 +77,24 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
         size.classList.add('selected');
     })
 });
+c('.pizzaInfo--addButton').addEventListener('click', () => {
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+    
+    let identifier = pizzaJson[modalKey].id + '@' + size;
+
+    let key = cart.findIndex((item) => item.identifier == identifier);
+   
+    if(key > -1){
+        cart[key].qt += modalQt;
+    } else{
+        cart.push({
+            identifier,
+            id: pizzaJson[modalKey].id,
+            size,
+            qt: modalQt
+        });
+    }
+
+    closeModal();
+});
+
