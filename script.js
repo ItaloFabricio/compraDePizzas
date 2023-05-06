d3e978ff62498188f64a99d101615d9f658d383d
ítalo Fabrício <italofps65@gmail.com>
2023-05-05 23:04:12 -0300

    feat: criando o modal

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ const cs = (el) => document.querySelectorAll(el);
 pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     //preencher as informações em pizzaitem
+    
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace('.', ',')}`;
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault(); //previna a ação padrão
+
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            c('.pizzaWindowArea').style.opacity = 1;
+        }, 200)
+        
+    });
 
     c('.pizza-area').append(pizzaItem);
 });
