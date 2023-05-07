cc226bcc4c4565eae9ef5cdf4262f2d83181b86b
ítalo Fabrício <italofps65@gmail.com>
2023-05-07 01:33:02 -0300

    feat: ajustando o carrinho no mobile e finalizando o projeto

--- script.js
+++ script.js
@@ -98,7 +98,19 @@ c('.pizzaInfo--addButton').addEventListener('click', () => {
     closeModal();
 });
 
+c('.menu-openner').addEventListener('click', () => {
+    if(cart.length > 0){
+        c('aside').style.left = 0;
+    }  
+});
+
+c('.menu-closer').addEventListener('click', () => {
+    c('aside').style.left = '100vw';
+})
+
 function updateCart(){
+    c('.menu-openner span').innerHTML = cart.length;
+
     if(cart.length > 0){
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
@@ -158,6 +170,7 @@ function updateCart(){
 
     } else{
         c('aside').classList.remove('show');
+        c('aside').style.left = '100vw';
     }
 }
 
